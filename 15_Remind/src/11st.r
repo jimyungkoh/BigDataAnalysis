da72f7b54563cf9b14b6aa6ce5fdbd1acf17ff6f
jimyungkoh <kojimyung1206@naver.com>
2021-06-05 23:38:45 +0900

    빅데이터 분석 11강 복습 내용 추가

--- 15_Remind/src/11st.r
+++ 15_Remind/src/11st.r
@@ -5,7 +5,8 @@
 #실습 1. 유럽 25개국 단백질 섭취율 데이터
 # protein.csv
 
-food <- read.csv("15_Remind/data/protein.csv")
+food <- read.csv("data/protein.csv")
+##read.csv("15_Remind/data/protein.csv")
 head(food)
 str(food)
 
@@ -125,3 +126,55 @@ table(ds$cluster, iris$Species)
 
 #시각화
 fviz_cluster(ds, iris2, geom = "point")
+
+########################################################
+## HierarcHical Clustering
+########################################################
+
+#실습 1. iris 데이터셋을 이용한 계층형 군집화
+
+iris2
+
+## 계층형 군집화 함수: hclust()
+## 거리 계산: dist() default=euclidean
+## 유사도 측정 방법: method 인자 값 -> single, complete, average
+
+hc <- hclust(dist(iris2), method= "average")
+hc
+
+##시각화
+
+plot(hc, lables = iris$Species, hang = -1)
+rect.hclust(hc, k=3)
+
+##클러스터 분할
+ghc <- cutree(hc, k=3)
+ghc
+table(iris$Species, ghc)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 실습 2. 계층형 군집화를 이용해 적절한 군집 수를 확인하고
+#   k-means 알고리즘을 적용해 군집별로 시각화
+
+## 데이터: ggplot2::diamonds
+dia <- ggplot2::diamonds
+dim(dia)
+
+## 1000개 샘플링
+t <- sample(1:nrow(dia), 1000)
+test <- dia[t,]
+dim(test)
+str(test)
+mydia <- test[c("price", "carat", "depth", "table")]
+head(mydia)
+
+## 계층형 군집화
+result <- hclust(dist(mydia), method="average")
+plot(result, hang=-1)    ### hang = -1 ->  line from the bottom
+
+##3개 군집화
+result2 <- kmeans(mydia, 3)
+
+##데이터에 클러스터 할당
+mydia$cluster <- result2$cluster
+head(mydia)
